5121459ffb69c0aba40bdbfd7a898592c447b120
jerni81 <jerni81@yahoo.com>
2019-09-24 10:30:43 -0400

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <div className="header">
+      <h1> Header </h1>
+      <div>
+        <h6>Hello, Brad!</h6>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(props) {
+  return (
+    <div className="main">
+      <h1> Main </h1>
+    </div>
+  )
+}
+
+export default Main;
