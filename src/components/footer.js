0f0b606e19f87ac313165010b5463f9e5b6b6aa3
jerni81 <jerni81@yahoo.com>
2019-09-24 11:34:46 -0400

    Footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,7 @@ import React from "react";
 function Footer(props) {
   return (
     <div className="footer">
+      <img src="/Users/aj/sei-pirates/uxProject/ux-project/src/images/desktop/socialMedia.jpg"></img>
       <a href="">About Us</a>
       <a href="">Community</a>
       <a href="">Contact</a>
