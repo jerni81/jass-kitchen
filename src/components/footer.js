2f15a415347d3ecf3b0e3275955aa8b91b5cff39
dyerjos <dyerjo@mail.gvsu.edu>
2019-09-24 13:59:23 -0400

    worked on header css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,15 @@
 import React from "react";
+import '../footer.css'
 
 function Footer(props) {
   return (
     <div className="footer">
 
-      <img  src ={process.env.PUBLIC_URL + 'images/desktop/social-media.png'}/>
-      <a href="">About Us</a>
-      <a href="">Community</a>
-      <a href="">Contact</a>
-      <a href="">Customer Support</a>
+      <img  src ={process.env.PUBLIC_URL + 'images/desktop/social-media.png'} alt="socialmedia"/>
+      <a href="/#">About Us</a>
+      <a href="/#">Community</a>
+      <a href="/#">Contact</a>
+      <a href="/#">Customer Support</a>
 
     </div>
   )
