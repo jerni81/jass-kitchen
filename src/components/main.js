ca77f2d85ea7d1a06382971df7663f4bf82c4c4d
dyerjos <dyerjo@mail.gvsu.edu>
2019-09-24 12:03:09 -0400

    added banner image

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,18 @@
 import React from "react";
+import '../main.css'
 
 function Main(props) {
   return (
     <div className="main">
+      <img
+        className="banner"
+        border="0"
+        alt="banner"
+        src ={process.env.PUBLIC_URL + 'images/desktop/banner.png'}
+        height="300px"
+      />
       <h1> Main </h1>
+
     </div>
   )
 }
