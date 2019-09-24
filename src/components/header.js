f02143555a4b8695c13e2ac002c0f539b1295974
jerni81 <jerni81@yahoo.com>
2019-09-24 12:32:19 -0400

    updates to header and footer

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,7 @@ function Header(props) {
   return (
     <div className="header">
        <a>JASS Kitchen</a>
+       <img  src ={process.env.PUBLIC_URL + 'images/desktop/shopping_cart.svg'}/>
     </div>
   )
 }
